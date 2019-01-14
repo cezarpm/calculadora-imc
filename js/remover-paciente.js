ea82c18ce8e03cacee258f7a6da7851b7bccb608
Cezar <cezar_pm@hotmail.com>
2019-01-14 05:51:20 -0200

    animação de remover paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -2,10 +2,14 @@
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick",function(event){
+    event.target.parentNode.classList.add("fadeOut")
+    
     var alvoEvento = event.target; 
     var paiDoAlvo = alvoEvento.parentNode;
-   
-    
-    paiDoAlvo.remove();
+
+    setTimeout(function(){
+        paiDoAlvo.remove();
+        
+    },500);
 
 });
